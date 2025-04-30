4fc8d4caad41376add61c829eff698701b0b2c7f
unknown <nelson.omueti.business@gmail.com>
2025-04-30 10:24:23 +0100

    commit

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 
 interface Product {
   _id: string;
@@ -13,8 +13,59 @@ interface Product {
   price: number;
 }
 
+const SearchComponent = () => {
+    const [products, setProducts] = useState([]);
+
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+      const searchTermFromUrl = searchParams.get("searchTerm");
+      if (searchTermFromUrl) {
+        axios
+          .get(`/api/search?searchTerm=${searchTermFromUrl}`)
+          .then((response) => setProducts(response.data.products))
+          .catch((error) =>
+            console.log("Error getting search result: ", error)
+          );
+      }
+    }, [searchParams]);
+
+    return (
+      <div
+        id="product"
+        className="px-4 md:px-12 py-5 md:py-10 flex justify-center items-center"
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2 lg-grid-cols-3 xl:grid-cols-4 gap-5">
+          {products.map((product: Product, index) => (
+            <Link href={`/product/${product._id}`} key={index}>
+              <Image
+                src={product.image}
+                height={1000}
+                width={1000}
+                alt="product image"
+                className="max-w-[17rem] h-72 object-cover object-center rounded-lg"
+              />
+
+              <div className="mt-4">
+                <h2 className="font-semibold text-lg">{product.name}</h2>
+                <p className="font-medium text-sm mt-1">${product.price}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+
 const SearchPage = () => {
 
+  return (
+    <Suspense fallback ={"Hello"}>
+      <SearchComponent/>
+    </Suspense>
+  )
+/*
     const [products, setProducts] = useState([]);
 
     const searchParams = useSearchParams();
@@ -52,6 +103,7 @@ const SearchPage = () => {
       </div>
     </div>
   );
-}
+}*/
+};
 
 export default SearchPage
